1056a5979c881c63f7a253b2978aac0885d47532
SoJune1023 <ssojune@naver.com>
2025-07-19 10:06:36 +0900

    feat: add jsonUtils.py (text_to_json(text_input), json_to_text(json_input))

---
+++ app/utils/jsonUtils.py
@@ -0,0 +1,13 @@
+import json
+
+def text_to_json(text_input):
+    """
+    chat-GPT 에게서 받은 text 파일을 json 형태로 가공합니다.
+    """
+    pass
+
+def json_to_text(json_input):
+    """
+    chat-GPT 에게 보낼 json 파일을 json 형태로 가공합니다.
+    """
+    pass
