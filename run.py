6b8bcf9ec2d4a2156bd310a1edbe671413e933f2
SoJune1023 <ssojune@naver.com>
2025-07-19 09:50:42 +0900

    first commit

---
+++ run.py
@@ -0,0 +1,12 @@
+import logging
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = "%(asctime)s [%(levelname)s] %(message)s",
+    handlers = [
+        logging.FileHandler("logs/logs.log", encoding = 'utf-8'),
+        logging.StreamHandler()
+    ]
+)
+
+logging.info("Hello, world!")
