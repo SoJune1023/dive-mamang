82a9207a3a9a6f4f9b6cf5b660ff933a2e059987
SoJune1023 <ssojune@naver.com>
2025-07-19 11:58:45 +0900

    feat: gpt_send() 기능 구현
    parse를 이용하여 json 출력을 강제, prompt와 message를 따로따로 받음.
    client, prompt, message를 받아야 작동

--- app/services/gpt.py
+++ app/services/gpt.py
@@ -11,8 +11,26 @@ class ResponseFormat(BaseModel):
 
 def gpt_setup_client():
     # TODO: set gpt client
+    # TODO: return client
     pass
 
-def gpt_send():
-    # TODO: send message to gpt
-    pass
+def gpt_send(client, prompt, message):
+    try:
+        response = client.responses.parse(
+            model = "model",
+            input = [
+                {
+                    "role": "system",
+                    "content": prompt
+                },
+                {
+                    "role": "user",
+                    "content": message
+                }
+            ],
+            text_format = ResponseFormat
+        )
+
+        # TODO: return result
+    except Exception as e:
+        raise Exception(f"Chat gpt did not response. Error code: {e}")
