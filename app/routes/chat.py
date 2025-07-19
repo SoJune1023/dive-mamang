e0305c1c58534060701c3a572cced7e21a937c26
SoJune1023 <ssojune@naver.com>
2025-07-19 10:03:04 +0900

    feat: add routes '/onSend' and todo.

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -1,1 +1,11 @@
-from flask import Flask
+from flask import blueprints
+
+chat_bp = blueprints('chat_bp', __name__)
+
+@chat_bp.routes('/onSend', methods = ['POST'])
+def onSend():
+    # TODO: chat-GPT 에게 보낼 text 파일을 user에게 받은 json에서 가공
+    # TODO: chat-GPT 세션 작성
+    # TODO: chat-GPT 에게 받은 text 파일을 json으로 가공
+    # TODO: return json
+    pass
