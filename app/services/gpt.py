86ee22639779e891f81df058082da74011b69ba3
SoJune1023 <ssojune@naver.com>
2025-07-19 12:15:31 +0900

    feat: gpt_send(client, model, prompt, message) 구현
    chat-GPT에게 message를 보내고 응답을 구조화 된 형태로 받습니다
    client, model, prompt, message의 value가 필요합니다.
    response_text 리스트와 image를 반환합니다.

--- app/services/gpt.py
+++ app/services/gpt.py
@@ -14,10 +14,29 @@ def gpt_setup_client():
     # TODO: return client
     pass
 
-def gpt_send(client, prompt, message):
+def gpt_send(client, model, prompt, message):
+    """
+    Chat-GPT에게 message를 보내고, 응답을 구조화된 형태로 받습니다.
+
+    Args:
+        client (OpenAI): chat-gpt의 client 인스턴스.
+        model (str) : 사용할 chat-gpt의 model.
+        prompt (str) : chat-gpt가 자신을 정의하는 prompt입니다.
+        message (str) : user가 chat-gpt에게 보낼 메시지.
+
+    Returns:
+        response.output_parsed: 다음 구조를 따릅니다:
+            - response_text (List[TextFormat]):
+                - message (str): 대화 내용
+                - context (str): 해당 메시지의 상황/맥락
+            - image (str): 이미지 URL
+
+    Raises:
+        RuntimeError: chat-gpt의 응답 받기에 실패한 경우.
+    """
     try:
         response = client.responses.parse(
-            model = "model",
+            model = model,
             input = [
                 {
                     "role": "system",
@@ -30,7 +49,6 @@ def gpt_send(client, prompt, message):
             ],
             text_format = ResponseFormat
         )
-
-        # TODO: return result
+        return response.output_parsed
     except Exception as e:
-        raise Exception(f"Chat gpt did not response. Error code: {e}")
+        raise RuntimeError("Failed to get response from chat-gpt") from e
