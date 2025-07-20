f17c9669bc1510cb94ed83f7092685c2dcdb98e9
SoJune1023 <ssojune@naver.com>
2025-07-20 19:01:20 +0900

    feat: add loadConfig.py
    data/user/config.json 에서 apikey, time, retires 등의 값을 불러옵니다.

---
+++ app/loadConfig.py
@@ -0,0 +1,18 @@
+import json
+import logging
+from pathlib import Path
+
+try:
+    path = Path(__file__).parent.parent / 'data' / 'user' / 'config.json'
+    with open(path, 'r', encoding = 'utf-8') as f:
+        data = json.load(f)
+except Exception as e:
+    #TODO: except시 error raises.
+    #TODO: log 남기기.
+    pass
+
+class Config:
+    # TODO: app/services/gpt.py의 gpt_setup_client()에 필요한 함수인 key, time, retires 불러오기.
+    # TODO: 위에서 불러온 값을 class를 활용하여 저장하기.
+    # TODO: log 남기기.
+    pass
