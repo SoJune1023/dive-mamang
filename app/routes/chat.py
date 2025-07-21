5539214a91929f624975d84426a8017c4f54ebeb
SoJune1023 <ssojune@naver.com>
2025-07-21 15:19:21 +0900

    feat: onSend() route 구현 완료.
    사용자로 부터 message와 user_note를 받습니다
    loadConfig.py에서 불러온 gpt의 설정값으로 client를 구성합니다
    prompt + user_note 후 client, message와 그것을 gpt에게 전송합니다
    gpt로 부터 응답을 받고 프론트엔드로 반환합니다.

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -1,4 +1,3 @@
-import json
 from flask import Blueprint, request, jsonify
 
 import utils
@@ -19,19 +18,17 @@ def onSend():
         """
 
         message = payload.get("message")
-        user_note = payload.get("user_note")
+        user_note = payload.get("user_note", "")
 
         prompt = utils.prompt_loader()
         gpt_prompt = utils.prompt_builder(prompt + user_note)
-
     except Exception as e:
-        pass
+        return jsonify({"error": "could not retrieve message or user_note"}), 403
 
     # API_KEY가 올바른 값인지 검증
     gpt_key = loadConfig.API_KEY
     if not gpt_key:
-        # TODO: raise error and skip the code.
-        pass
+        return jsonify({"error": "API_KEY is missing"}), 403
 
     gpt_model = loadConfig.MODEL
     gpt_time = loadConfig.MAX_TIME
@@ -40,6 +37,25 @@ def onSend():
     gpt_client = services.gpt_setup_client(gpt_key, gpt_time, gpt_retries)
     
     gpt_response = services.gpt_send(gpt_client, gpt_model, gpt_prompt, message)
+    """ gpt_response : dict
+    - response_text (List[TextFormat]):
+        - message (str): 대화 내용
+        - context (str): 해당 메시지의 상황/맥락
+    - image (str): 이미지 URL
+    """
 
-    # TODO: return json
-    pass
+    response_text = gpt_response.get("response_text")
+    response_image = gpt_response.get("image", "<default_image_URL>")
+
+    if not response_text:
+        return jsonify({"error": "Response is missing."}), 500
+
+    try:
+        result_text = ""
+        for text in response_text:
+            result_text += f'"{text["message"]}"'
+            result_text += f"*{text["context"]}*"
+
+        return jsonify({"text": result_text, "image": response_image}), 200
+    except Exception as e:
+        return jsonify({"error": "Could not load response from chat-gpt"}), 204
