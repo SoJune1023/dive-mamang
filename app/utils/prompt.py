c568e4e4f21643b8237aed9df6dd8bde5e71af4d
SoJune1023 <ssojune@naver.com>
2025-07-21 14:18:22 +0900

    feat: add app/utils/prompt.py
    prompt_loader() : load prompt from data/prompt/prompt.json
    prompt_builder() : get prompt and user_note, return final prompt.

---
+++ app/utils/prompt.py
@@ -0,0 +1,11 @@
+import json
+
+def prompt_loader():
+    # TODO: load prompt from json
+    pass
+
+def prompt_builder():
+    # TODO: get prompt and user note
+    # TODO: prompt + user note
+    # TODO: return result
+    pass
