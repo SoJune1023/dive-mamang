50acbfa88295e15d89800013cd570000171547f0
SoJune1023 <ssojune@naver.com>
2025-07-21 14:52:37 +0900

    feat: onSend route 구현중
    payload를 받아 user_note와 message 값 불러옴
    prompt를 불러온 후 prompt와 user_note를 합침
    gpt config value를 불러와 client 생성
    gpt에게 전송하여 결과를 반환받음

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -1,18 +1,45 @@
+import json
 from flask import Blueprint, request, jsonify
 
 import utils
+import services
+import loadConfig
 
 chat_bp = Blueprint('chat_bp', __name__)
 
-@chat_bp.routes('/onSend', methods = ['POST'])
+@chat_bp.route('/onSend', methods = ['POST'])
 def onSend():
-    # TODO: chat-GPT 에게 보낼 text 파일을 user에게 받은 json에서 가공
     try:
         payload = request.get_json(force = True)
+        """ payload : dict
+        {
+            "message"(str) : <user_message>,
+            "user_note"(str) : <user_note>
+        }
+        """
+
+        message = payload.get("message")
+        user_note = payload.get("user_note")
+
+        prompt = utils.prompt_loader()
+        gpt_prompt = utils.prompt_builder(prompt + user_note)
 
     except Exception as e:
         pass
-    # TODO: chat-GPT 세션 작성
-    # TODO: chat-GPT 에게 받은 text 파일을 json으로 가공
+
+    # API_KEY가 올바른 값인지 검증
+    gpt_key = loadConfig.API_KEY
+    if not gpt_key:
+        # TODO: raise error and skip the code.
+        pass
+
+    gpt_model = loadConfig.MODEL
+    gpt_time = loadConfig.MAX_TIME
+    gpt_retries = loadConfig.MAX_RETRIES
+
+    gpt_client = services.gpt_setup_client(gpt_key, gpt_time, gpt_retries)
+    
+    gpt_response = services.gpt_send(gpt_client, gpt_model, gpt_prompt, message)
+
     # TODO: return json
     pass
