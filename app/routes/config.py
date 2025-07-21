7ae599fa26c4bc147c65991e44d9f17e8592edd3
SoJune1023 <ssojune@naver.com>
2025-07-21 19:31:15 +0900

    feat: add route updateConfigValue()
    바꾸고자 하는 config value의 이름과 값을 payload로 get합니다.
    이것을 data/user/config.json에 dump 합니다.

--- app/routes/config.py
+++ app/routes/config.py
@@ -1,21 +1,43 @@
-from flask import Blueprint
+import json
+import logging
+from pathlib import Path
+from flask import Blueprint, request, jsonify
 
 config_bp = Blueprint('config_bp', __name__)
 
-# @config_bp.routes('/loadConfigValue', methods = ['POST'])
-# def loadConfigValue():
-#     # TODO: Load config value from data/user/config.json
-#     # TODO: Return config value to electron.
-#     pass
-
-@config_bp.routes('/updateConfigValue', methos = ['POST'])
+@config_bp.route('/updateConfigValue', methods = ['POST'])
 def updateConfigValue():
     # TODO: Get json
-    # payload : dict
-    # {
-    #     "willUpdate" : str,
-    #     "newValue" : str
-    # }
-    # TODO: update data/user/config.json
-    # TODO: return 0
-    pass
+    try:
+        payload = request.get_json(force = True)
+        """ payload : dict
+        {
+            "willUpdate" : str,
+            "newValue" : str
+        }
+        """
+        will_update = payload.get("willUpdate")
+        new_value = payload.get("new_value")
+
+        if not will_update or not new_value:
+            logging.info("Config value is missing.")
+            return jsonify({"error": "value is missing"}), 400
+    except Exception as e:
+        logging.info(f"Config value is missing. Error code: {e}")
+        return jsonify({"error": "value is missing"}), 400
+
+    try:
+        path = Path(__file__).parent.parent.parent / 'data' / 'user' / 'config.json'
+        with open(path, 'r', encoding = 'utf-8') as f:
+            data = json.load(f)
+        
+        data[will_update] = new_value
+
+        with open(path, 'w', encoding = 'utf-8') as f:
+            json.dump(data, f, ensure_ascii = False, indent = 4)
+
+        return jsonify({"message": "Config updated"}), 200
+
+    except Exception as e:
+        logging.info(f"Could not change config value. Error code: {e}")
+        return jsonify({"error": "Could not change config value"}), 500
