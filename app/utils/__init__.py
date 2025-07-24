0cf4035e080f0b43268666a625679f741359017b
SoJune1023 <ssojune@naver.com>
2025-07-24 15:15:32 +0900

    feat: import loadPrevious.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,4 +1,5 @@
 from .prompt import prompt_builder, prompt_loader
 from .jsonUtils import json_to_text
+from .loadPrevious import gpt_load_previous_conversation, user_load_previous_conversation
 
-__all__ = ['json_to_text', 'prompt_builder', 'prompt_loader']
+__all__ = ['json_to_text', 'prompt_builder', 'prompt_loader', 'gpt_load_previous_conversation', 'user_load_previous_conversation']
