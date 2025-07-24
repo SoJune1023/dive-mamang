659385de13858fd7ffaed3226c6add5d02aa7f6a
SoJune1023 <ssojune@naver.com>
2025-07-24 15:13:37 +0900

    feat: Add try문. 예외 처리.

--- app/utils/loadPrevious.py
+++ app/utils/loadPrevious.py
@@ -1,31 +1,40 @@
 import json
 import logging
 from pathlib import Path
+from flask import jsonify
 
 from app.loadConfig import Config
 
 def gpt_load_previous_conversation():
-    path = Path(__file__).parent.parent.parent / 'data' / 'user' / 'save.json'
-    with open(path, 'r', encoding = 'utf-8') as f:
-        data = json.load(f)
-    
-    previous_conversation = data['history']
-    # previous_conversation = [{"user": <conversation>, "gpt": <conversation>} . . .]
-    
-    # 과거 대화를 얼마나 가져올지 app/loadConfig.py에서 불러온 후 if 문을 통한 compare.
-    max_previous = Config.MAX_PREVIOUS
-    if len(previous_conversation) > max_previous:
-        # 최대 conversation history에 맞춤
-        previous_conversation[-max_previous:]
-    
-    return previous_conversation
+    try:
+        path = Path(__file__).parent.parent.parent / 'data' / 'user' / 'save.json'
+        with open(path, 'r', encoding = 'utf-8') as f:
+            data = json.load(f)
+        
+        previous_conversation = data['history']
+        # previous_conversation = [{"user": <conversation>, "gpt": <conversation>} . . .]
+        
+        # 과거 대화를 얼마나 가져올지 app/loadConfig.py에서 불러온 후 if 문을 통한 compare.
+        max_previous = Config.MAX_PREVIOUS
+        if len(previous_conversation) > max_previous:
+            # 최대 conversation history에 맞춤
+            previous_conversation[-max_previous:]
+        
+        return previous_conversation
+    except Exception as e:
+        logging.error(f"Failed to load previous conversation.\nFile: {__file__}\nError code: {e}")
+        return jsonify({"error": "Failed to load previous conversation."}), 403
 
 def user_load_previous_conversation():
-    path = Path(__file__).parent.parent.parent / 'data' / 'user' / 'save.json'
-    with open(path, 'r', encoding = 'utf-8') as f:
-        data = json.load(f)
-    
-    previous_conversation = data['history']
-    # previous_conversation = [{"user": <conversation>, "gpt": <conversation>} . . .]
+    try:
+        path = Path(__file__).parent.parent.parent / 'data' / 'user' / 'save.json'
+        with open(path, 'r', encoding = 'utf-8') as f:
+            data = json.load(f)
+        
+        previous_conversation = data['history']
+        # previous_conversation = [{"user": <conversation>, "gpt": <conversation>} . . .]
 
-    return previous_conversation
+        return previous_conversation
+    except Exception as e:
+        logging.error(f"Failed to load previous conversation.\nFile: {__file__}\nError code: {e}")
+        return jsonify({"error": "Failed to load previous conversation."}), 403
