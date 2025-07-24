7dd3b0088b0fccb982a8dc35ad826d0d1afa327b
SoJune1023 <ssojune@naver.com>
2025-07-24 11:13:14 +0900

    feat: MODEL config value에 기본값 추가 및 logging 남발 삭제

--- app/loadConfig.py
+++ app/loadConfig.py
@@ -11,11 +11,6 @@ except Exception as e:
 
 class Config:
     API_KEY = data.get("API_KEY", "none")
-    logging.info(f"Success to load API_KEY.\nAPI_KEY = {API_KEY}")
-    MODEL = data.get("MODEL")
-    logging.info(f"Success to load MODEL.\nMODEL = {MODEL}")
+    MODEL = data.get("MODEL", "gpt-4o")
     MAX_TIME = data.get("TIMEOUT", 120)
-    logging.info(f"Success to load MAX_TIME.\nMODEMAX_TIMEL = {MAX_TIME}")
     MAX_RETRIES = data.get("MAX_RETRIES", 2)
-    logging.info(f"Success to load MAX_RETRIES.\nMAX_RETRIES = {MAX_RETRIES}")
-    pass
