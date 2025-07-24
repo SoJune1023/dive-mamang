ad4aa76642625650d090ab30c7452c7967c6c2b0
SoJune1023 <ssojune@naver.com>
2025-07-24 15:07:34 +0900

    feat: gpt_load_previous_conversation 및 user용 ^그긴거^ 구현
    data/user/save.json에서 일정 길이의 previous conversation을 load 하여 return 합니다.
    config.json에서 가져올 과거 대화의 최대 길이: MAX_PREVIOUS

--- app/utils/loadPrevious.py
+++ app/utils/loadPrevious.py
@@ -4,14 +4,29 @@ from pathlib import Path
 
 from app.loadConfig import Config
 
-def load_previous_conversation():
+def gpt_load_previous_conversation():
     path = Path(__file__).parent.parent.parent / 'data' / 'user' / 'save.json'
     with open(path, 'r', encoding = 'utf-8') as f:
         data = json.load(f)
     
     previous_conversation = data['history']
+    # previous_conversation = [{"user": <conversation>, "gpt": <conversation>} . . .]
     
-    # TODO: 과거 대화를 얼마나 가져올지 app.loadConfig 파일에서 불러온 후 if 문을 통한 compare.
-    previous_conversation = previous_conversation[-10:]
+    max_previous = Config.MAX_PREVIOUS
 
-    # TODO: return result
+    # 과거 대화를 얼마나 가져올지 app/loadConfig.py에서 불러온 후 if 문을 통한 compare.
+    if len(previous_conversation) > max_previous:
+        # 최대 conversation history에 맞춤
+        previous_conversation[-max_previous:]
+    
+    return previous_conversation
+
+def user_load_previous_conversation():
+    path = Path(__file__).parent.parent.parent / 'data' / 'user' / 'save.json'
+    with open(path, 'r', encoding = 'utf-8') as f:
+        data = json.load(f)
+    
+    previous_conversation = data['history']
+    # previous_conversation = [{"user": <conversation>, "gpt": <conversation>} . . .]
+
+    return previous_conversation
