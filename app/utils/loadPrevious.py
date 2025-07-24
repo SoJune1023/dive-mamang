1be80b8d3cc806adacc8f93c2ed03c4a164e0d91
SoJune1023 <ssojune@naver.com>
2025-07-24 15:09:05 +0900

    chore: 주석 위치 수정

--- app/utils/loadPrevious.py
+++ app/utils/loadPrevious.py
@@ -12,9 +12,8 @@ def gpt_load_previous_conversation():
     previous_conversation = data['history']
     # previous_conversation = [{"user": <conversation>, "gpt": <conversation>} . . .]
     
-    max_previous = Config.MAX_PREVIOUS
-
     # 과거 대화를 얼마나 가져올지 app/loadConfig.py에서 불러온 후 if 문을 통한 compare.
+    max_previous = Config.MAX_PREVIOUS
     if len(previous_conversation) > max_previous:
         # 최대 conversation history에 맞춤
         previous_conversation[-max_previous:]
