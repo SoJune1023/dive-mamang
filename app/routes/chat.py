26cf8404f48457b7713b83aad8747a661bd287c6
SoJune1023 <ssojune@naver.com>
2025-08-10 01:00:20 +0900

    refactor: 아주 약간의 성능 개선

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -82,10 +82,9 @@ def onSend():
 
     # 만약 img_list에 gpt_response img가 없을 경우 default img를 가져옵니다.
     img_list = utils.load_img_list()
-    defalut_img = utils.load_default_img()
     response_image = gpt_response.get("image")
     if not response_image in img_list:
-        response_image = defalut_img
+        response_image = utils.load_default_img()
 
     # Gpt response를 user가 읽기 쉬운 형태로 반환
     try:
